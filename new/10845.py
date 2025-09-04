25c72607d5e484d817b03ee4c8be8feeac44215e
doremihyeon <algus04617@gmail.com>
2025-09-04 23:43:33 +0900

    retry

--- new/10845.py
+++ new/10845.py
@@ -43,3 +43,30 @@ for _ in range(N):
             result.append(-1)
 
 print(*result, sep="\n")
+
+# deque 방법으로 푸는법
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+
+# N = int(input())
+# queue = deque()
+# result = []
+
+# for _ in range(N):
+#     cmd = input().split()
+
+#     if cmd[0] == "push":
+#         queue.append(int(cmd[1]))
+#     elif cmd[0] == "pop":
+#         result.append(queue.popleft() if queue else -1)
+#     elif cmd[0] == "size":
+#         result.append(len(queue))
+#     elif cmd[0] == "empty":
+#         result.append(0 if queue else 1)
+#     elif cmd[0] == "front":
+#         result.append(queue[0] if queue else -1)
+#     elif cmd[0] == "back":
+#         result.append(queue[-1] if queue else -1)
+
+# print("\n".join(map(str, result)))
