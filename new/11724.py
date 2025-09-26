d79f9bf4fbd6a013e67b7a926e619dc9798b1026
doremihyeon <algus04617@gmail.com>
2025-09-26 17:37:19 +0900

    yet

---
+++ new/11724.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[]for _ in range(M+1)]
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
