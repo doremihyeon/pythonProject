c1b732e3d2729d56acfbee7aac588de27ee29797
doremihyeon <algus04617@gmail.com>
2025-09-29 16:14:20 +0900

    again

--- new/11724.py
+++ new/11724.py
@@ -3,10 +3,23 @@ input = sys.stdin.readline
 
 N, M = map(int, input().split())
 graph = [[]for _ in range(M+1)]
+visited = [False] * (N+1)
 
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
 
+def dfs(v):
+    visited[v] = True
+    for next in graph[v]:
+        if not visited[next]:
+            dfs(next)
 
+count = 0
+for i in range(1, N+1):
+    if not visited[i]:
+        dfs(i)
+        count += 1
+    
+print(count)
